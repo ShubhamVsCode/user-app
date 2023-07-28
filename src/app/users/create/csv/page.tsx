1069c8d88409125f0befd0e587bf2208bc596d7b
Shubham <shubhamvscode@gmail.com>
2023-07-28 14:38:27 +0530

    CSV Sample

--- src/app/users/create/csv/page.tsx
+++ src/app/users/create/csv/page.tsx
@@ -15,6 +15,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { userSchema } from "@/schema/userSchema";
 import FileComponent from "@/components/FileComponent";
 import CreateUserForm from "@/components/CreateUserForm";
+import Link from "next/link";
+import { Download } from "lucide-react";
 
 const CreatePage = () => {
   const { usersFromCSV } = useSelector(usersSelector);
@@ -24,6 +26,25 @@ const CreatePage = () => {
       <h1 className="text-3xl font-semibold text-center mt-7 mb-5">
         Create User from CSV
       </h1>
+      <div className="text-center mb-5 flex justify-center items-center gap-2">
+        <Link
+          href={"/SampleUserData.csv"}
+          className="border border-black/30 rounded-md px-2 py-1 bg-transparent hover:bg-slate-200 duration-300 flex group"
+          download={true}
+        >
+          <Download />
+        </Link>
+
+        <Link
+          href={
+            "https://docs.google.com/spreadsheets/d/1g8rT2JJfECY8s2drXypp-l2BQOI-VkKVZ6xR9JXCHx8/edit?usp=sharing"
+          }
+          className="border border-black/30 rounded-md px-3 py-1 bg-transparent hover:bg-slate-200 duration-300"
+          target="_blank"
+        >
+          Sample File
+        </Link>
+      </div>
 
       <div className="max-w-lg mx-auto">
         <FileComponent />
